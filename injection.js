61297676b1aaf0b84956a8a63b0ce1d3094fe619
Its Hiro <wow.itshiro@gmail.com>
2024-11-20 21:30:16 +0700

    Update injection.js

--- injection.js
+++ injection.js
@@ -566,7 +566,7 @@ function modifyCode(text) {
 			});
 
 			// Killaura
-			let attackDelay = Date.now() - 9999999;
+			let attackDelay = Date.now() - 99999999;
 			let didSwing = false;
 			let attacked = 0;
 			let attackedPlayers = {};
